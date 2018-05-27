efd7c8c9c0df06b9091af64de5ab3e29588d4025
杨丰林 <1490719714@qq.com>
2018-05-27 12:02:03 +0800

    初始化项目

---
+++ YFL-IOS/YFL-IOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YFL-IOS
+//
+//  Created by 杨丰林 on 2018/5/27.
+//  Copyright © 2018年 杨丰林. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YFL-IOS/YFL-IOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YFL-IOS
+//
+//  Created by 杨丰林 on 2018/5/27.
+//  Copyright © 2018年 杨丰林. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
